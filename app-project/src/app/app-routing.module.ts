f790e092aae7676b34740eab3cf2235b981f7973
Matheus <matheusalcantaradelima1997@gmail.com>
2019-06-13 15:46:46 -0300

    rotas configuradas e página de personagens adicionada

--- app-project/src/app/app-routing.module.ts
+++ app-project/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { PersonagensComponent } from './personagens/personagens.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component: HomeComponent},
+  {path:'personagens', component: PersonagensComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
